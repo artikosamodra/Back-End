cd2ba412d791871dd0be25de03beed7d9b78a7ee
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-15 17:31:26 +0700

    Server.js (First FIle)
    this first file from js

---
+++ Web-Service-1/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+ 
+    const { method } = request;
+ 
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        response.end('<h1>Hai!</h1>');
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+};
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
