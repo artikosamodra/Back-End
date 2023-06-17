5c9de829349abd789a5d157c7c094d861f293ad7
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-18 00:54:41 +0700

    Update routes.js (Request Params)
    menambahkan request params untuk antisipasi pengakses tidak memiliki username

--- hapi-webserver-1/routes.js
+++ hapi-webserver-1/routes.js
@@ -1,4 +1,6 @@
 const routes = [
+
+        //method GET untuk url '/'
         {
                 method: 'GET',
                 path: '/',
@@ -7,11 +9,49 @@ const routes = [
                 },
         },
 
+        //method not GET (POST, PUT, DELETE) untuk url '/'
+        {
+                method: '*',
+                path: '/',
+                handler: (request, h) => {
+                        return 'Halaman tidak dapat diakses dengan method tersebut';
+                },
+        },
+
+        //method GET untuk url '/about'
         {
                 method: 'GET',
                 path: '/about',
                 handler: (request, h) => {
                         return 'About Page';
+                },
+        },
+
+        //method not GET (POST, PUT, DELETE) untuk url '/about'
+        {
+                method: '*',
+                path: '/about',
+                handler: (request, h) => {
+                        return 'Halaman tidak dapat diakses dengan method tersebut';
+                },
+        },
+
+        //request params
+        {
+                method: 'GET',
+                path: '/hello/{name?}',
+                handler: (request, h) => {
+                        const {name = "guest"} = request.params; //jika tidak terdapat username
+                        return `Hello, ${name}!`;
+                },
+        },
+        
+        //method not GET (POST, PUT, DELETE) untuk url selain '/' & '/about'
+        {
+                method: '*',
+                path: '/{any*}',
+                handler: (request, h) => {
+                        return 'Halaman tidak ditemukan';
                 },
         },
 ];
