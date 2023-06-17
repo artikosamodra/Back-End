fd4d2405f3ddd25eae2816638e4b5d846f8ba2b9
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-17 23:58:06 +0700

    Update server.js (HTTP server - hapi framework)
    menggunakan framework hapi untuk membuat http.

--- hapi-webserver-1/server.js
+++ hapi-webserver-1/server.js
@@ -1,1 +1,13 @@
-console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi'); //menggunakan framework hapi untuk membuat http
+
+const init - async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host" 'localhost',
+	});
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.url}`);
+}
+
+init();
