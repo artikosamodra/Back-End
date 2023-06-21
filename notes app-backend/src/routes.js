e3f328d99d25944af72c40cf56c88b0532050c42
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-21 13:48:11 +0700

    Update routes.js (DELETE)
    Menambah method `DELETE` untuk memanggil handler deleteNoteByIdHandler pada file handler.js

--- notes app-backend/src/routes.js
+++ notes app-backend/src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler, } = require('./handler');
 
 const routes = [
   {
@@ -23,6 +23,12 @@ const routes = [
     method: 'PUT',
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
+  },
+
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 
 ];
