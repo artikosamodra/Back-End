d05097d1d48b24ff3530930e8f06f2d751ad28a7
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-21 13:47:23 +0700

    Update handler.js (deleteNoteByIdHandler)
    menambahkan handler `deleteNoteByIdHandler` untuk opsi menu delete catatan.

--- notes app-backend/src/handler.js
+++ notes app-backend/src/handler.js
@@ -98,4 +98,27 @@ const editNoteByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, };
+//deleteNoteByIdHandler
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1){
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal duhapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
