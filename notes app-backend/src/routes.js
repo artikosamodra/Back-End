10f729150324723cdf8359b9114d68abd959a4ab
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-21 08:43:53 +0700

    Update routes.js (add fungsi handler)
    menambahkan addNoteHandler sebagai handler pada method POST pada route.

---
+++ notes app-backend/src/routes.js
@@ -0,0 +1,13 @@
+const {addNoteHandler} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  
+    },
+  },
+];
+
+module.exports = routes;
