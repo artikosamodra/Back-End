97656538bd7f3b040998b19bc6d8d4c5a58fc0c8
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-06-21 11:57:06 +0700

    Update routes.js (add method)
    Add method GET untuk allnote dan notebyid

--- notes app-backend/src/routes.js
+++ notes app-backend/src/routes.js
@@ -1,12 +1,24 @@
-const {addNoteHandler} = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
   
-    },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  }
+
 ];
 
 module.exports = routes;
